de066f23c822e4aa27d18dbb2cd1922a5f6c0e9d
laowu <whzlong@163.com>
2014-09-22 23:34:02 +0800

    add

--- src/src/com/whzlong/anke/LoadActivity.java
+++ src/src/com/whzlong/anke/LoadActivity.java
@@ -3,17 +3,26 @@ package com.whzlong.anke;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class LoadActivity extends Activity {
 
+	private final int LOAD_TIME = 6000;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_load);
 		
-		Intent intent = new Intent();
-		intent.setClass(LoadActivity.this, MainActivity.class);
-		startActivity(intent);
-		LoadActivity.this.finish();
+		new Handler().postDelayed(new Runnable(){
+			public void run(){
+				Intent intent = new Intent();
+				intent.setClass(LoadActivity.this, MainActivity.class);
+				startActivity(intent);
+				LoadActivity.this.finish();
+			}
+		}, LOAD_TIME);
+		
+		
+		
 	}
 }
