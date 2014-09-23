574285d8a42f4cdae24079638a6f18b04c17b18a
laowu <whzlong@163.com>
2014-09-24 00:25:04 +0800

    ok

--- src/src/com/whzlong/anke/LoadActivity.java
+++ src/src/com/whzlong/anke/LoadActivity.java
@@ -1,28 +1,58 @@
 package com.whzlong.anke;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Toast;
 
 public class LoadActivity extends Activity {
 
-	private final int LOAD_TIME = 6000;
+	private final int LOAD_TIME = 1000;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_load);
 		
-		new Handler().postDelayed(new Runnable(){
-			public void run(){
-				Intent intent = new Intent();
-				intent.setClass(LoadActivity.this, MainActivity.class);
-				startActivity(intent);
-				LoadActivity.this.finish();
-			}
-		}, LOAD_TIME);
+		Context context = this.getApplicationContext();
 		
-		
+		if(isNetworkConnected(context)){
+			//跳转至主界面
+			new Handler().postDelayed(new Runnable(){
+				public void run(){				
+					Intent intent = new Intent();
+					intent.setClass(LoadActivity.this, MainActivity.class);
+					startActivity(intent);
+					LoadActivity.this.finish();
+				}
+			}, LOAD_TIME);
+		}else{
+			Toast.makeText(context, "网络连接不可用", Toast.LENGTH_LONG).show();
+		}
 		
 	}
+	
+	/**
+	 * 是否有网络连接
+	 * @param context
+	 * @return
+	 */
+	public boolean isNetworkConnected(Context context) {
+		if (context != null) {
+			ConnectivityManager mConnectivityManager = (ConnectivityManager) context
+					.getSystemService(Context.CONNECTIVITY_SERVICE);
+			NetworkInfo mNetworkInfo = mConnectivityManager
+					.getActiveNetworkInfo();
+			if (mNetworkInfo != null) {
+				return mNetworkInfo.isAvailable();
+			}
+		}
+		
+		return false;
+	} 
+	
 }
--- src/src/com/whzlong/anke/MainActivity.java
+++ src/src/com/whzlong/anke/MainActivity.java
@@ -1,13 +1,51 @@
 package com.whzlong.anke;
 
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
-
+	private Button btnQuit = null;
+			
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		btnQuit = (Button)findViewById(R.id.btnQuit);
+		btnQuit.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				ActivityManager am = (ActivityManager)getSystemService (Context.ACTIVITY_SERVICE); 
+				am.killBackgroundProcesses(getPackageName());
+			}
+		});
 	}
+	
+//	private OnClickListener listener = new OnClickListener() {
+//		public void onClick(View view) {
+//			switch (view.getId()) {
+//			case R.id.btnEnergySavingData:
+//				break;
+//			case R.id.btnRealTimeData:
+//				break;
+//			case R.id.btnSystemSet:
+//				break;
+//			case R.id.btnQuit:
+//				ActivityManager am = (ActivityManager)getSystemService (Context.ACTIVITY_SERVICE); 
+//				am.killBackgroundProcesses(getPackageName());
+//				System.exit(0);
+//				break;
+//			default:
+//				break;
+//			}
+//		}
+//	};
+
 }
