26f27566a09b01474908fe539635a483e3224531
whzlong <whzlong@163.com>
2014-09-28 00:04:44 +0800

    table

--- src/src/com/whzlong/anke/EnergySavingDataActivity.java
+++ src/src/com/whzlong/anke/EnergySavingDataActivity.java
@@ -4,15 +4,38 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageButton;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class EnergySavingDataActivity extends Activity {
+	private final int WC = ViewGroup.LayoutParams.WRAP_CONTENT;
+
+	private final int FP = ViewGroup.LayoutParams.FILL_PARENT;
 	private Button btnBack = null;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_energy_saving_data);
+		
+//		TableLayout tablelayout = (TableLayout)findViewById(R.id.tlEnergySavingData);
+//		
+//		tablelayout.setStretchAllColumns(true);
+//		
+//		for(int row = 0; row < 30; row++){
+//			TableRow tableRow = new TableRow(this);
+//			
+//			for(int col=0; col < 20; col++){
+//				TextView tv = new TextView(this);
+//				tv.setText("("+col+","+row+")");
+//				tableRow.addView(tv);
+//			}
+//			
+//			tablelayout.addView(tableRow, new TableLayout.LayoutParams(FP,WC));
+//		}
+		
 		
 		btnBack = (Button)findViewById(R.id.btnBack);
 		btnBack.setOnClickListener(new Button.OnClickListener() {
