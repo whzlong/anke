98a1139cb7ac44479795ded789e6554ea0c06ce1
laowu <whzlong@163.com>
2014-10-27 10:33:26 +0800

    adjust date picker

--- src/src/com/whzlong/anke/activity/EnergySavingData.java
+++ src/src/com/whzlong/anke/activity/EnergySavingData.java
@@ -308,8 +308,6 @@ public class EnergySavingData extends BaseActivity implements OnClickListener,
 			cal.setTimeInMillis(System.currentTimeMillis());
 			datePicker.init(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH),
 					cal.get(Calendar.DAY_OF_MONTH), null);
-
-			
 			
 			switch (v.getId()) {
 			case R.id.btnBack:
