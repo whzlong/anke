0e1bdde11032f05aff71838ab2c1a5f29d089303
whzlong <whzlong@163.com>
2015-01-17 17:03:58 +0800

    adjusted ui

--- src/src/com/whzlong/anke/activity/BaseActivity.java
+++ src/src/com/whzlong/anke/activity/BaseActivity.java
@@ -9,6 +9,7 @@ import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.Window;
 
 public class BaseActivity extends Activity {
 
@@ -26,6 +27,7 @@ public class BaseActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		allowFullScreen = true;
 		// 添加Activity到堆栈
 		AppManager.getAppManager().addActivity(this);
--- src/src/com/whzlong/anke/activity/Main.java
+++ src/src/com/whzlong/anke/activity/Main.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.Window;
 import android.widget.Button;
 
 public class Main extends BaseActivity implements OnClickListener {
